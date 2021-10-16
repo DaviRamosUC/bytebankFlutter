5eadf7f2493bc339d21de0b43956c383e591290b
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-10-16 11:48:54 -0300

    Finalizando Persistencia

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:bytebank_2/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Bytebank());
+}
+
+class Bytebank extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.green[900],
+          secondary: Colors.blueAccent[700],
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
