e698922a0fe61ecc3b95ce091449a1cbaaa6224f
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-10-24 21:03:02 -0300

    Refatorando bug de uso do context

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -67,14 +67,10 @@ class _TransactionFormState extends State<TransactionForm> {
                           Transaction(value!, widget.contact);
                       showDialog(
                           context: context,
-                          builder: (context) {
+                          builder: (contextDialog) {
                             return TransactionAuthDialog(
                               onConfirm: (password) {
-                                _webClient
-                                    .save(transactionCreated, password)
-                                    .then((transaction) {
-                                  Navigator.pop(context);
-                                });
+                                _save(transactionCreated, password, context);
                               },
                             );
                           });
@@ -87,5 +83,15 @@ class _TransactionFormState extends State<TransactionForm> {
         ),
       ),
     );
+  }
+
+  void _save(
+    Transaction transactionCreated,
+    String password,
+    BuildContext context,
+  ) async {
+    _webClient.save(transactionCreated, password).then((transaction) {
+      Navigator.pop(context);
+    });
   }
 }
