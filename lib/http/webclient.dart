a16830f66289982da1571422511974104e5c880a
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-10-27 21:04:41 -0300

    Fazendo detalhamento do erro para o Crashlytics

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -8,4 +8,4 @@ final Client client = InterceptedClient.build(
   requestTimeout: Duration(seconds: 5),
 );
 
-const String baseUrl = 'http://192.168.1.102:8080/transactions';
+const String baseUrl = 'http://192.168.1.102:8080/transaction';
