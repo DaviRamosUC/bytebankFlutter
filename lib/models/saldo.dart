4aeac25bf64026bb2f1d9d0e0080de397e3049f7
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-11-01 13:14:59 -0300

    Alterando dashboad

--- lib/models/saldo.dart
+++ lib/models/saldo.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/cupertino.dart';
 
 class Saldo extends ChangeNotifier{
-  final double valor;
+  double valor;
 
   Saldo(this.valor);
+
+  void adiciona(double valor){
+    this.valor += valor;
+    notifyListeners();
+  }
+
+  void subtrai(double valor){
+    if(valor<=this.valor){
+      this.valor -= valor;
+      notifyListeners();
+    }
+  }
 
   @override
   String toString() {
--- lib/screens/dashboard/dashboardSec.dart
+++ lib/screens/dashboard/dashboardSec.dart
@@ -1,17 +1,46 @@
+import 'package:bytebank_2/models/saldo.dart';
 import 'package:bytebank_2/screens/dashboard/saldoCard.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DashboardSec extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Bytebank'),
-      ),
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: SaldoCard(),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Bytebank'),
+        ),
+        body: ListView(children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: SaldoCard(),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 24),
+            child: Consumer<Saldo>(
+              builder: (context, saldo, child) {
+                return ElevatedButton(
+                  child: Text('Adicionar'),
+                  onPressed: () {
+                    saldo.adiciona(11.0);
+                  },
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 24),
+            child: Consumer<Saldo>(
+              builder: (context, saldo, child) {
+                return ElevatedButton(
+                  child: Text('Subtrair'),
+                  onPressed: () {
+                    saldo.subtrai(11.0);
+                  },
+                );
+              },
+            ),
+          )
+        ]));
   }
 }
